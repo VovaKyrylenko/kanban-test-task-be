08259d8851c23c254f1fa867754f50ddc98f957a
Volodymyr Kyrylenko <vovichkyry@gmail.com>
2024-03-17 19:59:05 +0200

    Updated Task interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,6 +2,7 @@ export interface ITask extends Document {
   _id: string;
   title: string;
   description: string;
+  status: 'ToDo' | 'In Progress' | 'Done';
 }
 
 export interface IBoard extends Document {
