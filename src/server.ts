326c2cfe0a6ef5b0cead82d8b46bff5a82f64917
Volodymyr Kyrylenko <vovichkyry@gmail.com>
2024-03-26 21:16:16 +0200

    Added CORS configs

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import { boardRouter, taskRouter } from './routers/';
 import { connectDB } from './database';
 
@@ -6,6 +7,8 @@ const app = express();
 const PORT = 6666;
 
 app.use(express.json());
+app.use(cors());
+
 connectDB();
 app.use('/api/boards', boardRouter);
 app.use('/api/tasks', taskRouter);
